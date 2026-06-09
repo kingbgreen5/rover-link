18c53c1832e38bf5883069a2b9b5e0f7b59f901e
Brian King <Thebeakeh@gmail.com>
2026-06-09 14:06:43 -0500

    pathing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,18 +6,10 @@ import desktopBanner from "/assets/Desktop Banner2-cropped.png";
 const Header = () => {
     return (
         <header className ="header">
-{/*   
-         <img className='header-img-desktop'    src="../../assets/Desktop Banner2-cropped.png" alt="" />
-         <img className='header-img-mobile'    src="../../assets/Mobile Banner.png" alt="" />
-
-          */}
 
 <img className='header-img-mobile' src={mobileBanner} alt="" />
 <img className='header-img-desktop' src={desktopBanner} alt="" />
 
-         {/* <div>  <h1>    Red Planet Explorer</h1>
-         <h3>Images from Rovers Curiosity and Perseverance</h3>
-         </div> */}
 
             </header>
     );
